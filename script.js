12296eb0f0448f164c0792fbe91b92baf3ea9018
Jasmine Schrewelius <jassersoet@hotmail.com>
2022-11-14 14:09:08 +0100

    First request

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function loadWheaterFromServer(){
-    <script src="https://api.openweathermap.org/data/2.5/weather?lat={56.0467}&lon={12.6944}&appid={1dd63c8198d8911de01c8b5aca6880bc}"></script>
-    let wheater = JSON.parse(response);
-    console.log(wheater);
-}
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://api.openweathermap.org/geo/1.0/direct?q=Helsingborg&limit=5&appid=1dd63c8198d8911de01c8b5aca6880bc', true); 
+xhr.responseType = 'json';
+xhr.send();
+
+xhr.onload = function() {
+  let response = xhr.response;
+  let longitude = response[0].lon;
+  let latitude = response[0].lat;
+  let name = response[0].name;
+  console.log(name, latitude, longitude)
+};
+
+/*
+let Http = new XMLHttpRequest();
+Http.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=1dd63c8198d8911de01c8b5aca6880bc&units=metric', true); 
+Http.responseType = 'json';
+Http.send();
+*/
