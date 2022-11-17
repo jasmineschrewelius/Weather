91cefbca67d050caa6f5a64b6077bd9773b83f5f
Jasmine Schrewelius <jassersoet@hotmail.com>
2022-11-17 14:11:35 +0100

    Last Commit

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
+//Hämta longitud och latitud från API
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://api.openweathermap.org/geo/1.0/direct?q=Helsingborg&limit=5&appid=1dd63c8198d8911de01c8b5aca6880bc', true); 
 xhr.responseType = 'json';
 xhr.send();
 
+// Skapa variabler , spara  ner long, lat och stad
 xhr.onload = function() {
   let response = xhr.response;
   let longitude = response[0].lon;
   let latitude = response[0].lat;
   let name = response[0].name;
-  let newObject = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=1dd63c8198d8911de01c8b5aca6880bc&units=metric';
-  
-  let object1 = new XMLHttpRequest();
-  object1.open('GET', newObject , true); 
-  object1.responseType = 'json';
-  object1.send();
 
-  console.log(object1);
+  // Skapa en ny URL med hjälp av koordinaterna
+  let newURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=1dd63c8198d8911de01c8b5aca6880bc&units=metric';
   
-  object1.onload = function() {
-  let response = object1.response;
+  // Hämta temperatur och vädret från API
+  let weather1 = new XMLHttpRequest();
+  weather1.open('GET', newURL , true); 
+  weather1.responseType = 'json';
+  weather1.send();
+  
+  // Skapa variabler , spara  ner temperatur och vädret
+  weather1.onload = function() {
+  let response = weather1.response;
   let temp = response.main.temp;
   let weather = response.weather[0].main;
-  console.log(temp , weather)
 
+// Skriv ut informationen på skärm för användaren
 document.getElementById("row1").innerHTML = "The local weather in" + " " + name + " " + ", Sweden.";
 
 document.getElementById("row2").innerHTML = temp + " C " + weather
